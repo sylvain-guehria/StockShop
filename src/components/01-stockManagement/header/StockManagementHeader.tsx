d9cf1a590b38e3c113c30f4d43ad45c404dd9c05
Sylvain Guehria <sylvain.guehria@gmail.com>
2022-10-20 12:51:20 +0200

    feat(marketplace): remove mistake copy paste

--- src/components/01-stockManagement/header/StockManagementHeader.tsx
+++ src/components/01-stockManagement/header/StockManagementHeader.tsx
@@ -9,9 +9,6 @@ import { Fragment } from 'react';
 
 const StockManagementHeader = () => {
   return (
-    <div className="mx-auto max-w-7xl px-4 sm:px-6">
-    <div className="flex items-center justify-between border-b border-gray-200 py-4 md:justify-start md:space-x-10">
-      <div className="flex justify-start lg:w-0 lg:flex-1">
     <div className="flex flex-1 justify-between px-4 sm:px-6 lg:mx-auto lg:max-w-6xl lg:px-8">
       <div className="flex flex-1">
         <form className="flex w-full md:ml-0" action="#" method="GET">
@@ -116,8 +113,6 @@ const StockManagementHeader = () => {
           </Transition>
         </Menu>
       </div>
-    </div>
-    </div>
     </div>
   );
 };
--- src/layouts/StockManagementLayout.tsx
+++ src/layouts/StockManagementLayout.tsx
@@ -12,27 +12,25 @@ const StockManagementLayout: FC<IBackgroundProps> = ({ children }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="h-full bg-gray-100">
-      <div className="h-full">
-        <div className="min-h-full">
-          <StockManagementSideBar
-            sidebarOpen={sidebarOpen}
-            setSidebarOpen={setSidebarOpen}
-          />
-          <div className="flex flex-1 flex-col lg:pl-64">
-            <div className="flex h-16 shrink-0 border-b border-gray-200 bg-white lg:border-none">
-              <button
-                type="button"
-                className="border-r border-gray-200 px-4 text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-500 lg:hidden"
-                onClick={() => setSidebarOpen(true)}
-              >
-                <span className="sr-only">Open sidebar</span>
-                <Bars3CenterLeftIcon className="h-6 w-6" aria-hidden="true" />
-              </button>
-              <StockManagementHeader />
-            </div>
-            {children}
+    <div className="h-screen bg-gray-100">
+      <div className="min-h-full">
+        <StockManagementSideBar
+          sidebarOpen={sidebarOpen}
+          setSidebarOpen={setSidebarOpen}
+        />
+        <div className="flex flex-1 flex-col lg:pl-64">
+          <div className="flex h-16 shrink-0 border-b border-gray-200 bg-white lg:border-none">
+            <button
+              type="button"
+              className="border-r border-gray-200 px-4 text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-500 lg:hidden"
+              onClick={() => setSidebarOpen(true)}
+            >
+              <span className="sr-only">Open sidebar</span>
+              <Bars3CenterLeftIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+            <StockManagementHeader />
           </div>
+          {children}
         </div>
       </div>
     </div>
