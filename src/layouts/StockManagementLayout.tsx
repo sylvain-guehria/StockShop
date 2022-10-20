255b48084ac93accc671d04c8177933e2b7be01a
Sylvain Guehria <sylvain.guehria@gmail.com>
2022-10-20 12:54:55 +0200

    feat(marketplace): simplify layout stockmanagement

--- src/layouts/StockManagementLayout.tsx
+++ src/layouts/StockManagementLayout.tsx
@@ -1,4 +1,3 @@
-import { Bars3CenterLeftIcon } from '@heroicons/react/24/outline';
 import type { FC, ReactNode } from 'react';
 import { useState } from 'react';
 
@@ -19,17 +18,7 @@ const StockManagementLayout: FC<IBackgroundProps> = ({ children }) => {
           setSidebarOpen={setSidebarOpen}
         />
         <div className="flex flex-1 flex-col lg:pl-64">
-          <div className="flex h-16 shrink-0 border-b border-gray-200 bg-white lg:border-none">
-            <button
-              type="button"
-              className="border-r border-gray-200 px-4 text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-500 lg:hidden"
-              onClick={() => setSidebarOpen(true)}
-            >
-              <span className="sr-only">Open sidebar</span>
-              <Bars3CenterLeftIcon className="h-6 w-6" aria-hidden="true" />
-            </button>
-            <StockManagementHeader />
-          </div>
+          <StockManagementHeader setSidebarOpen={setSidebarOpen} />
           {children}
         </div>
       </div>
