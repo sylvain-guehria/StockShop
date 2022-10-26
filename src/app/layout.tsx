afe9bf6728fba13c3a2f5adb8306fb08a9de36e5
Sylvain Guehria <sylvain.guehria@gmail.com>
2022-10-27 01:23:41 +0200

    feat(nextjs 13): create the /app folder with layout example + migrate tailwind

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import '../styles/global.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}', './app/**/*.{js,ts,jsx,tsx}'],
   theme: {
     fontSize: {
       xs: ['0.75rem', { lineHeight: '1rem' }],
