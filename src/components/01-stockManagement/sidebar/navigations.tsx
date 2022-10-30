aea8412fca909fdec26cb5095aa805541dfe1900
Sylvain Guehria <sylvain.guehria@gmail.com>
2022-10-30 15:14:36 +0100

    feat(): add a link to go to marketplace when in the dashbord

---
+++ src/components/01-stockManagement/sidebar/navigations.tsx
@@ -0,0 +1,30 @@
+import {
+  ClockIcon,
+  CogIcon,
+  CreditCardIcon,
+  DocumentChartBarIcon,
+  HomeIcon,
+  QuestionMarkCircleIcon,
+  ScaleIcon,
+  ShieldCheckIcon,
+  ShoppingBagIcon,
+  UserGroupIcon,
+} from '@heroicons/react/24/outline';
+
+export const navigation = [
+  { name: 'Home', href: '#', icon: HomeIcon, current: true },
+  { name: 'History', href: '#', icon: ClockIcon, current: false },
+  { name: 'Balances', href: '#', icon: ScaleIcon, current: false },
+  { name: 'Cards', href: '#', icon: CreditCardIcon, current: false },
+  { name: 'Recipients', href: '#', icon: UserGroupIcon, current: false },
+  { name: 'Reports', href: '#', icon: DocumentChartBarIcon, current: false },
+];
+export const secondaryNavigation = [
+  { name: 'Settings', href: '#', icon: CogIcon },
+  { name: 'Help', href: '#', icon: QuestionMarkCircleIcon },
+  { name: 'Privacy', href: '#', icon: ShieldCheckIcon },
+];
+
+export const thirdNavigation = [
+  { name: 'Marketplace', href: '/marketplace', icon: ShoppingBagIcon },
+];
