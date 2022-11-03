95d682c4fc0447047b28c9144e6f3c4561b90868
Sylvain Guehria <sylvain.guehria@gmail.com>
2022-11-03 18:42:18 +0000

    feat(dev environment): setup the emulators clients + admin

--- firebaseFolder/clientApp.ts
+++ firebaseFolder/clientApp.ts
@@ -2,6 +2,7 @@
 import { initializeApp } from 'firebase/app';
 import {
   confirmPasswordReset,
+  connectAuthEmulator,
   createUserWithEmailAndPassword,
   deleteUser,
   FacebookAuthProvider,
@@ -17,6 +18,7 @@ import {
 } from 'firebase/auth';
 import {
   collection,
+  connectFirestoreEmulator,
   deleteDoc,
   doc,
   getDoc,
@@ -28,6 +30,7 @@ import {
   where,
 } from 'firebase/firestore';
 import {
+  connectStorageEmulator,
   deleteObject,
   getDownloadURL,
   getStorage,
@@ -50,6 +53,10 @@ const app = initializeApp({ ...firebaseConfig });
 const auth = getAuth(app);
 const firestore = getFirestore(app);
 const storage = getStorage(app);
+
+connectFirestoreEmulator(firestore, 'localhost', 8080);
+connectAuthEmulator(auth, 'http://localhost:9099');
+connectStorageEmulator(storage, 'localhost', 9199);
 
 const firestoreFunctions = {
   doc,
--- firebaseFolder/serverApp.ts
+++ firebaseFolder/serverApp.ts
@@ -14,6 +14,13 @@ if (!admin.apps.length) {
     console.error('Firebase admin initialization error', error.stack);
   }
 }
+
+if (process.env.ENV === 'local') {
+  process.env.FIRESTORE_EMULATOR_HOST = 'localhost:8080';
+  process.env.FIREBASE_AUTH_EMULATOR_HOST = 'localhost:9099';
+  process.env.FIREBASE_STORAGE_EMULATOR_HOST = 'localhost:9199';
+}
+
 const firebaseAdmin = admin;
 export default firebaseAdmin;
 
