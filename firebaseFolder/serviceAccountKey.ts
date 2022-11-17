f03ee5b0240980498c8109145c343a57df11e611
Sylvain Guehria <sylvain.guehria@gmail.com>
2022-11-17 08:12:28 +0000

    feat(firebase): try to set up firebase admin with env variable

---
+++ firebaseFolder/serviceAccountKey.ts
@@ -0,0 +1,16 @@
+/* eslint-disable import/prefer-default-export */
+export const serviceAccount = {
+  type: 'service_account',
+  project_id: 'stockshop-8006f',
+  private_key_id: '6520fd514178671487b37ee936ceed1041a94fed',
+  private_key:
+    '-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCoy7aB3Rp6QUWz\nA0KUYGEJfIyKV+gHhipuP160rEaVDbG0qj1GTKeIL+hSR0WTOiXrCtZgXoT7PKun\nyTAUKrfcR3j1iL1pLBt3tNNWTaX4n7fX0Ft9CnBnjxKLyYgzzYWfkdXEjNSDD5Bm\ncqvIj9hu/suVB0rSwTxUQxcj9aQoCezhwzHu/iGNQd3yRiCkc/VpJI1R7sdTtUqu\nei0ndufOOUFgnwSHWlSHfhFtLO6RTVPlPtIhJghzorCvcMdTz2VoXc2j+uPBBavg\ndSPvZtp9XriEza/cmonTik6EaQWHUJT75aPfXeasUNgrfY/BnHQ4FROQGjZZ5gKi\nho5+CObnAgMBAAECggEABdbaREl8/4AKObmh+tZktPp5NYCkrGdLSM9qEXpTjMIn\nBMwjh4Q3rXc+zw3bzc6OqOq8j3k673gMq3wYbNzXdHLdL91K9Cb295//9EsxCStO\npal1vhsiLVWekMsS1PvwpJxbwUtqlJ1xY4yRqG61euLXRxtepBt+4J6w375r2d4D\noMfc2SZ8NyRx/WLf4kbvXTs8gAd0UKnKRPceWlACBhZI6jvBrnR0lo/wZprPrV4A\nwz0yzZ4OjZNId8bkqAlji1Tm0KSOtA9ACkwRAgJn8FKxWbHSxeWPH1DbEgVSRClI\nuM9KuEtvCmuVwXRZn2PHmA6sSIV8GxgSCR4Z14797QKBgQDbtBrY7/6R0urxMmuj\nDrOvVdOpmhkR29isjqR8lxLTTGx6Ia5Cplp9TjCk+/sPOJCgGA599fch0JJePpxP\ntXfGJVQgFk71Zup3auiz3No6haYXNrWmDzaxNJQfP1hs+xMEzmbUKSH0b/69GreF\ncyfrb+TncUiPGcQiZlci3l3b4wKBgQDErpJG2kzsmx5u1ETdZWfc+CEjL0laSzqq\nbBWtZC5eiCBCxjHc+Uec615KOT1bqoZMuCJg/JVaauzch9L4IApSYO3YqeCfth6U\nxXsz0rRz2RZUveFVgbLZB8AcUFS+/22fTENzoz8zpkJ3PQZbLxNVhICbBRTXy61L\nRv2vcxXALQKBgCscgFgpX87FkIyb1Ds+20uAj/Qr5Nd4SGAz7Vr1WiBUUsT0bvlt\nIzyLZ0g4md82vkpr7gwRg9kC0HXl205Y0ZP0JznUptHIzCF6vQbuQ3RQXXMYrTRU\nr+q94oLvLy7TZbv/tHXVTHBmuZnp8DEjk6XDYyt3M0HBUJdfdaHF+Kz5AoGAPBpI\nswQku60PWLjfLnS9j4X3wmPB26jfjZKn8SuLC/I3QZCFTkyS+L3oO3BQcsGidrFS\nOCcXkODZ7u5GpvJLlS7vn+URslyZ7caHRqfd4wT00QKRaGrQ94dpyn012IP/wM0y\nU5UQTD1DVGK+nYC+PMTX9AdSfjtMgpN4FHRrsiUCgYA5gnf46HJp8T5ag0WNe4oW\nl5+ru4OpCTt4miBTUfE2mNhxYNArXKDtRTjAryI+m6EygIw2uB4Y53MRtG16nV9N\ncHMoizjr+8NoGSQNUQlfPcV1wuApelJAK0Swy1vBZcKh0XySlZjL1vGf/8uIrYqu\nCEr3Ytn+S7rCupTa4D5a2A==\n-----END PRIVATE KEY-----\n',
+  client_email:
+    'firebase-adminsdk-eikv4@stockshop-8006f.iam.gserviceaccount.com',
+  client_id: '107390615861429427075',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-eikv4%40stockshop-8006f.iam.gserviceaccount.com',
+};
