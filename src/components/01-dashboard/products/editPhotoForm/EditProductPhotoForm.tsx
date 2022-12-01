cbdc5f6a018795bdab22f29fdb60d000d7316f45
Sylvain Guehria <sylvain.guehria@gmail.com>
2022-12-01 12:01:00 +0000

    feat(photo product): remove spinner and set photoUrl on fetch success

--- src/components/01-dashboard/products/editPhotoForm/EditProductPhotoForm.tsx
+++ src/components/01-dashboard/products/editPhotoForm/EditProductPhotoForm.tsx
@@ -9,7 +9,6 @@ import { useForm } from 'react-hook-form';
 
 import LinkButton from '@/components/04-lib/LinkButton/LinkButton';
 import NextImage from '@/components/04-lib/nextImage/NextImage';
-import Spinner from '@/components/04-lib/spinner/Spinner';
 import { ToasterTypeEnum } from '@/components/08-toaster/toasterEnum';
 import { useAuth } from '@/hooks/useAuth';
 import { useToast } from '@/hooks/useToast';
@@ -37,8 +36,9 @@ const EditProductPhotoForm: FC<Props> = ({
   const { user } = useAuth();
   const toast = useToast(5000);
   const [currentFile, setCurrentFile] = useState<File | null>(null);
+  const [imagePreviewUrl, setImagePreviewUrl] = useState<string>('');
 
-  const { data: product, isLoading: isLoadingProduct } = useQuery({
+  const { data: product } = useQuery({
     queryKey: ['get-product', { productUid }],
     queryFn: () =>
       productRepository.getById({
@@ -48,11 +48,10 @@ const EditProductPhotoForm: FC<Props> = ({
         inventoryUid,
       }),
     enabled: !!productUid,
+    onSuccess: (data) => {
+      setImagePreviewUrl(data?.getPhotoLink());
+    },
   });
-
-  const [imagePreviewUrl, setImagePreviewUrl] = useState<string>(
-    product?.getPhotoLink() || ''
-  );
 
   const fileInput: React.RefObject<HTMLInputElement> = createRef();
 
@@ -135,81 +134,77 @@ const EditProductPhotoForm: FC<Props> = ({
           Visuel du produit
         </h3>
       </div>
-      {isLoadingProduct ? (
-        <Spinner />
-      ) : (
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="mt-6 sm:col-span-6">
-            <div className="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
-              <div className="space-y-1 text-center">
-                <div className="mb-3">
-                  {imagePreviewUrl ? (
-                    <NextImage
-                      src={imagePreviewUrl}
-                      alt="current product photo"
-                      width={200}
-                      height={200}
-                    />
-                  ) : (
-                    <svg
-                      className="mx-auto h-12 w-12 text-gray-400"
-                      stroke="currentColor"
-                      fill="none"
-                      viewBox="0 0 48 48"
-                      aria-hidden="true"
-                    >
-                      <path
-                        d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                        strokeWidth={2}
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                    </svg>
-                  )}
-                </div>
-
-                <div className="w-full text-sm text-gray-600">
-                  <input
-                    ref={fileInput}
-                    accept={'.png, .jpg, .jpeg'}
-                    id="file-upload"
-                    name="file-upload"
-                    type="file"
-                    className="sr-only"
-                    onChange={handleImageChange}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mt-6 sm:col-span-6">
+          <div className="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
+            <div className="space-y-1 text-center">
+              <div className="mb-3">
+                {imagePreviewUrl ? (
+                  <NextImage
+                    src={imagePreviewUrl}
+                    alt="current product photo"
+                    width={200}
+                    height={200}
                   />
-                  <div className="text-sm text-gray-600">
-                    <LinkButton type="button" onClick={() => handleClick()}>
-                      {imagePreviewUrl ? 'Changer' : 'Ajouter'}
-                    </LinkButton>
-                    {imagePreviewUrl && (
-                      <LinkButton type="button" onClick={() => handleRemove()}>
-                        Supprimer
-                      </LinkButton>
-                    )}
-                  </div>
-                </div>
-                <p className="text-xs text-gray-500">PNG, JPG, JPEG max 2MB</p>
-                {errors.size?.message && (
-                  <p className="text-sm text-red-600" id="inventoryName-error">
-                    {errors.size?.message}
-                  </p>
-                )}
-                {errors.type?.message && (
-                  <p className="text-sm text-red-600" id="inventoryName-error">
-                    {errors.type?.message}
-                  </p>
+                ) : (
+                  <svg
+                    className="mx-auto h-12 w-12 text-gray-400"
+                    stroke="currentColor"
+                    fill="none"
+                    viewBox="0 0 48 48"
+                    aria-hidden="true"
+                  >
+                    <path
+                      d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                      strokeWidth={2}
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
                 )}
               </div>
+
+              <div className="w-full text-sm text-gray-600">
+                <input
+                  ref={fileInput}
+                  accept={'.png, .jpg, .jpeg'}
+                  id="file-upload"
+                  name="file-upload"
+                  type="file"
+                  className="sr-only"
+                  onChange={handleImageChange}
+                />
+                <div className="text-sm text-gray-600">
+                  <LinkButton type="button" onClick={() => handleClick()}>
+                    {imagePreviewUrl ? 'Changer' : 'Ajouter'}
+                  </LinkButton>
+                  {imagePreviewUrl && (
+                    <LinkButton type="button" onClick={() => handleRemove()}>
+                      Supprimer
+                    </LinkButton>
+                  )}
+                </div>
+              </div>
+              <p className="text-xs text-gray-500">PNG, JPG, JPEG max 2MB</p>
+              {errors.size?.message && (
+                <p className="text-sm text-red-600" id="inventoryName-error">
+                  {errors.size?.message}
+                </p>
+              )}
+              {errors.type?.message && (
+                <p className="text-sm text-red-600" id="inventoryName-error">
+                  {errors.type?.message}
+                </p>
+              )}
             </div>
           </div>
-          <div>
-            <LinkButton type="submit" style="tertiary">
-              Sauvegarder
-            </LinkButton>
-          </div>
-        </form>
-      )}
+        </div>
+        <div>
+          <LinkButton type="submit" style="tertiary">
+            Sauvegarder
+          </LinkButton>
+        </div>
+      </form>
     </div>
   );
 };
