3c7031a663538770d6555ca3d5c4ba7a9707000f
Sylvain Guehria <sylvain.guehria@gmail.com>
2022-12-15 14:22:22 +0000

    feat(): bg-gray-200 for every elements in LP

--- src/components/06-template/Banner.tsx
+++ src/components/06-template/Banner.tsx
@@ -5,7 +5,7 @@ import LinkButton from '../04-lib/LinkButton/LinkButton';
 
 const Banner = () => (
   <Section yPadding="pb-16">
-    <div className="flex flex-col rounded-md bg-primary-100 p-4 text-center sm:flex-row sm:items-center sm:justify-between sm:p-12 sm:text-left">
+    <div className="flex flex-col rounded-md bg-gray-200 p-4 text-center sm:flex-row sm:items-center sm:justify-between sm:p-12 sm:text-left">
       <div className="text-2xl font-semibold">
         <div className="mb-2 text-gray-900">
           Gérer votre inventaire et vendre vos produits en ligne n&apos;a jamais
