e7e073fce5c9b7d9b47c9507153ce3f26db9e4e2
Sylvain Guehria <sylvain.guehria@gmail.com>
2022-12-20 08:18:24 +0000

    feat(): put first connection modal in the page because of hydration bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,13 @@
 import '../styles/global.css';
 
-import { userRepository } from 'di';
 import type { JSXElementConstructor, ReactElement } from 'react';
 import React from 'react';
-
-import FirstConnectionModalWithProviders from '@/components/05-modals/FirstConnectionModal';
-import type UserEntity from '@/modules/user/UserEntity';
-import { validateUser } from '@/utils/validateUserServerSide';
 
 const RootLayout = async ({
   children,
 }: {
   children: ReactElement<any, string | JSXElementConstructor<any>>;
 }) => {
-  const uid = await validateUser();
-
-  let user: UserEntity | null = null;
-
-  if (uid) {
-    user = await userRepository.getById(uid);
-    if (user.needToSeeFirstConnectionModal()) {
-      return <FirstConnectionModalWithProviders />;
-    }
-  }
-
   return (
     <html lang="en">
       <head>
