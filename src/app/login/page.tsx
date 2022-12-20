d27e451a6655ca2be6e7be34ac42cf4adfef208b
Sylvain Guehria <sylvain.guehria@gmail.com>
2022-12-20 08:22:13 +0000

    feat(): redirect user register and login with middlware

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,6 @@
-import { redirect } from 'next/navigation';
-
 import LoginComponant from '@/components/03-auth/login/LoginComponant';
-import { mainRoutes } from '@/routes/mainRoutes';
-import { validateUser } from '@/utils/validateUserServerSide';
 
-const Login = async () => {
-  const uid = await validateUser();
-  if (uid) {
-    redirect(mainRoutes.home.path);
-  }
+const Login = () => {
   return <LoginComponant />;
 };
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -9,13 +9,24 @@ import { mainRoutes } from './routes/mainRoutes';
 export function middleware(request: NextRequestType) {
   const sessionCookie = request.cookies.get(sessionCookieName);
   const { pathname } = request.nextUrl;
-  if (!sessionCookie) {
-    return NextResponse.redirect(new URL(mainRoutes.login.path, request.url));
+
+  if (pathname.includes(inventoryManagementRoutes.dashboard.path)) {
+    if (!sessionCookie) {
+      return NextResponse.redirect(new URL(mainRoutes.login.path, request.url));
+    }
+    if (pathname === inventoryManagementRoutes.dashboard.path) {
+      return NextResponse.redirect(
+        new URL(inventoryManagementRoutes.myInventory.path, request.url)
+      );
+    }
   }
-  if (pathname === inventoryManagementRoutes.dashboard.path) {
-    return NextResponse.redirect(
-      new URL(inventoryManagementRoutes.myInventory.path, request.url)
-    );
+
+  if (
+    (pathname === mainRoutes.login.path ||
+      pathname === mainRoutes.register.path) &&
+    sessionCookie
+  ) {
+    return NextResponse.redirect(new URL(mainRoutes.home.path, request.url));
   }
 
   return NextResponse.next();
@@ -23,5 +34,9 @@ export function middleware(request: NextRequestType) {
 
 // Supports both a single string value or an array of matchers
 export const config = {
-  matcher: ['/dashboard/:path*'],
+  matcher: [
+    '/dashboard/:path*',
+    mainRoutes.login.path,
+    mainRoutes.register.path,
+  ],
 };
