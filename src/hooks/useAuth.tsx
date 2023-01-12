3a811f059505f3eeebf87809ce98e2bd711ff83d
Sylvain Guehria <sylvain.guehria@gmail.com>
2023-01-12 07:56:23 +0000

    feat(): use react query to retrieve user

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,21 +1,27 @@
+import { useQuery } from '@tanstack/react-query';
 import { userRepository } from 'di';
 import { sessionCookieName } from 'firebaseFolder/constant';
 import Cookies from 'js-cookie';
 import React, { createContext, useContext, useEffect, useState } from 'react';
 
+import { ApiRequestEnums } from '@/enums/apiRequestEnums';
 import UserEntity from '@/modules/user/UserEntity';
 
 import { auth, onAuthStateChanged } from '../../firebaseFolder/clientApp';
 import { isFirebaseUserFirstConnexion } from './hooksUtils';
 
+const oneHourInMilliseconds = 1000 * 60 * 60;
+
 type ContextType = {
   user: UserEntity;
   setUser: (user: UserEntity) => void;
+  isUserLoading: boolean;
 };
 
 const AuthContext = createContext<ContextType>({
   user: UserEntity.new(),
   setUser: () => {},
+  isUserLoading: false,
 });
 
 export const useAuth = () => useContext(AuthContext);
@@ -26,22 +32,28 @@ export const AuthContextProvider = ({
   children: React.ReactNode;
 }) => {
   const [user, setUser] = useState<UserEntity>(UserEntity.new());
+  const [userUid, setUserUid] = useState<string>('');
+
+  const { isLoading: isUserLoading } = useQuery({
+    queryKey: [ApiRequestEnums.GetUser],
+    queryFn: () => userRepository.getById(userUid),
+    enabled: !!userUid,
+    onSuccess: (retrievedUser) => {
+      console.log('retrievedUser---------------------', retrievedUser);
+      setUser(retrievedUser);
+    },
+    onError: (error) => {
+      // eslint-disable-next-line no-console
+      console.error('Error userRepository.getById in useAuth', error);
+    },
+    staleTime: oneHourInMilliseconds,
+  });
 
   useEffect(() => {
-    const fetchUserInformation = async (uid: string) => {
-      try {
-        return await userRepository.getById(uid);
-      } catch (e) {
-        // eslint-disable-next-line no-console
-        console.error('ERROR fetchUserInformation', e);
-        return null;
-      }
-    };
-
     const unsubscribe = onAuthStateChanged(auth, async (firebaseUser) => {
       if (!firebaseUser) {
         Cookies.remove(sessionCookieName);
-        setUser(UserEntity.new({}));
+        setUserUid('');
         return;
       }
 
@@ -55,14 +67,13 @@ export const AuthContextProvider = ({
         return;
       }
 
-      const fetchedUser = await fetchUserInformation(firebaseUser.uid);
-      setUser(UserEntity.new(fetchedUser || {}));
+      setUserUid(firebaseUser?.uid || '');
     });
     return () => unsubscribe();
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ user, setUser, isUserLoading }}>
       {children}
     </AuthContext.Provider>
   );
