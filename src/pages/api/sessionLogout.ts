de3735d21a7a47209a681dbbe240a47e25e2c73d
Sylvain Guehria <sylvain.guehria@gmail.com>
2023-01-15 10:07:32 +0000

    feat(): donot need to remove token, supabase do it bi itself

--- src/pages/api/sessionLogout.ts
+++ src/pages/api/sessionLogout.ts
@@ -1,4 +1,4 @@
-import { getCookie, removeCookies } from 'cookies-next';
+import { getCookie } from 'cookies-next';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { superBaseAuthTokenCookieName } from 'supabase/constant';
 import { supabaseAdmin } from 'supabase/server/adminApp';
@@ -9,8 +9,6 @@ const sessionLogout = async (req: NextApiRequest, res: NextApiResponse) => {
   if (!sessionCookie) return res.status(400).end('No session cookie');
 
   supabaseAdmin.signOut(sessionCookie as string);
-
-  removeCookies(superBaseAuthTokenCookieName, { req, res });
 
   return res.status(200).end('Session Cookie deleted');
 };
