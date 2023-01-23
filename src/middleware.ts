e92e62008e0e42fabdd484b745691fe21a44b3b0
Sylvain Guehria <sylvain.guehria@gmail.com>
2023-01-23 16:40:55 +0000

    feat(): check if user in logedin in the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,22 @@
 import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import type { NextRequest as NextRequestType } from 'next/server';
 import { NextResponse } from 'next/server';
-import { superBaseAuthTokenCookieName } from 'supabase/constant';
 
 import { inventoryManagementRoutes } from './routes/inventoryManagementRoutes';
 import { mainRoutes } from './routes/mainRoutes';
 
 export async function middleware(req: NextRequestType) {
-  const sessionCookie = req.cookies.get(superBaseAuthTokenCookieName);
   const { pathname } = req.nextUrl;
 
+  const res = NextResponse.next();
+
+  const supabase = createMiddlewareSupabaseClient({ req, res });
+
+  const session = await supabase.auth.getSession();
+  const user = session.data.session?.user;
+
   // LOGGEDIN USER
-  if (sessionCookie) {
+  if (user) {
     if (
       pathname.startsWith(mainRoutes.login.path) ||
       pathname.startsWith(mainRoutes.register.path) ||
@@ -22,16 +27,11 @@ export async function middleware(req: NextRequestType) {
   }
 
   // VISITOR USER
-  if (!sessionCookie) {
+  if (!user) {
     if (pathname.startsWith(inventoryManagementRoutes.dashboard.path)) {
       return NextResponse.redirect(new URL(mainRoutes.login.path, req.url));
     }
   }
-  const res = NextResponse.next();
-
-  const supabase = createMiddlewareSupabaseClient({ req, res });
-
-  await supabase.auth.getSession();
 
   return res;
 }
