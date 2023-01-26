607951975ebaa962246cf21dcbc7ae8a5ae62e44
Sylvain Guehria <sylvain.guehria@gmail.com>
2023-01-26 17:48:07 +0000

    feat(): refac get inventory api

--- src/pages/api/inventory/getInventoriesByCompanyId.ts
+++ src/pages/api/inventory/getInventoriesByCompanyId.ts
@@ -1,28 +1,34 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import createServerSupabaseSSRClient from 'supabase/server/supabase-ssr';
+import { TableNames } from 'supabase/tables/tableNames';
 
 const getInventoriesByCompanyId = async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
   const {
-    query: { userId, companyId },
-    method,
+    query: { companyId },
   } = req;
 
-  try {
-    switch (method) {
-      case 'GET':
-        res.status(200).json({ userId, companyId });
-        return;
-      default:
-        res.setHeader('Allow', ['GET']);
-        res.status(405).end(`Method ${method} Not Allowed`);
-    }
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(e);
-    res.status(400).end('[]');
+  if (!companyId) {
+    res.status(400).end('companyId is mandatory to get inventories');
+    return [];
   }
+
+  const supabaseSsr = createServerSupabaseSSRClient({ req, res });
+
+  const { error, data } = await supabaseSsr
+    .from(TableNames.INVENTORIES)
+    .select('*')
+    .eq('companyId', companyId);
+
+  if (error) {
+    // eslint-disable-next-line no-console
+    console.error('error when adding a company', error);
+    res.status(400).end();
+    return [];
+  }
+  return res.status(200).json(data);
 };
 
 export default getInventoriesByCompanyId;
