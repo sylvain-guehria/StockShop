51ba7cd465fbcb6e1f8f84fdb27b15a146fe8214
Sylvain Guehria <sylvain.guehria@gmail.com>
2023-02-04 09:54:47 +0000

    feat(): delete photo of a deleted product

--- src/usecases/product/deleteProduct.spec.ts
+++ src/usecases/product/deleteProduct.spec.ts
@@ -1,27 +1,117 @@
+import { BucketNames } from 'supabase/tables/bucketNames';
+
+import ProductEntity from '@/modules/product/ProductEntity';
+
 import { deleteProduct } from './deleteProduct';
 
 const productRepository = {
   delete: jest.fn(),
 };
 
+const supabaseStorage = {
+  from: jest.fn().mockReturnThis(),
+  remove: jest.fn().mockReturnThis(),
+};
+
+const productWithPhoto = ProductEntity.new({
+  id: 'productId',
+  photoLink: 'photoLink',
+  inventoryId: 'inventoryId',
+});
+
+const productWithoutPhoto = ProductEntity.new({
+  id: 'productId',
+  photoLink: '',
+  inventoryId: 'inventoryId',
+});
+
 describe('deleteProduct', () => {
-  it('Do not delete the product if the productId is not provided', async () => {
-    const productId = '';
+  it('Do not delete the product if the product is not provided', async () => {
+    const companyId = 'companyId';
 
     try {
-      await deleteProduct(productRepository as any)(productId);
+      await deleteProduct(
+        productRepository as any,
+        supabaseStorage as any
+        // @ts-ignore
+      )({ product: null, companyId });
+    } catch (error: any) {
+      expect(productRepository.delete).toHaveBeenCalledTimes(0);
+      expect(error.message).toBe('product is required to delete the product');
+    }
+  });
+
+  it('Do not delete the product if the product has no Id', async () => {
+    const companyId = 'companyId';
+
+    try {
+      await deleteProduct(
+        productRepository as any,
+        supabaseStorage as any
+      )({
+        product: ProductEntity.new({
+          photoLink: 'photoLink',
+        }),
+        companyId,
+      });
     } catch (error: any) {
       expect(productRepository.delete).toHaveBeenCalledTimes(0);
       expect(error.message).toBe('productId is required to delete the product');
     }
   });
 
-  it('Delete the product', async () => {
-    const productId = 'productId';
+  it('Do not delete the product if the companyId is not provided', async () => {
+    try {
+      await deleteProduct(
+        productRepository as any,
+        supabaseStorage as any
+      )({ product: productWithPhoto, companyId: '' });
+    } catch (error: any) {
+      expect(productRepository.delete).toHaveBeenCalledTimes(0);
+      expect(error.message).toBe('companyId is required to delete the product');
+    }
+  });
 
-    await deleteProduct(productRepository as any)(productId);
+  it('Delete a product without photo', async () => {
+    const companyId = 'companyId';
+
+    productRepository.delete.mockResolvedValue(true);
+
+    await deleteProduct(
+      productRepository as any,
+      supabaseStorage as any
+    )({ product: productWithoutPhoto, companyId });
 
     expect(productRepository.delete).toHaveBeenCalledTimes(1);
-    expect(productRepository.delete).toHaveBeenCalledWith(productId);
+    expect(productRepository.delete).toHaveBeenCalledWith(
+      productWithoutPhoto.getId()
+    );
+    expect(supabaseStorage.from).toHaveBeenCalledTimes(0);
+    expect(supabaseStorage.remove).toHaveBeenCalledTimes(0);
+  });
+
+  it('Delete a product with photo and delete the photo', async () => {
+    const companyId = 'companyId';
+
+    productRepository.delete.mockResolvedValue(true);
+    supabaseStorage.from(BucketNames.PRODUCTS).remove.mockResolvedValue({
+      data: [{ metadata: { httpStatusCode: 200 } }],
+    });
+
+    await deleteProduct(
+      productRepository as any,
+      supabaseStorage as any
+    )({ product: productWithPhoto, companyId });
+
+    expect(productRepository.delete).toHaveBeenCalledTimes(1);
+    expect(productRepository.delete).toHaveBeenCalledWith(
+      productWithPhoto.getId()
+    );
+    expect(
+      supabaseStorage.from(BucketNames.PRODUCTS).remove
+    ).toHaveBeenCalledTimes(1);
+    expect(
+      supabaseStorage.from(BucketNames.PRODUCTS).remove
+    ).toHaveBeenCalledWith(['companyId/inventoryId/productId']);
   });
 });
