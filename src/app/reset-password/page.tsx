90687d2e6b71f7987201c761e3a108ddea1fd421
Sylvain Guehria <sylvain.guehria@gmail.com>
2023-02-04 13:36:47 +0000

    feat(): delete useless page

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,7 +1,7 @@
-import ResetPasswordPage from './resetPasswordPage';
+import ResetPasswordComponant from '@/components/03-auth/reset-password/ResetPasswordComponant';
 
 const resetPassword = async () => {
-  return <ResetPasswordPage />;
+  return <ResetPasswordComponant />;
 };
 
 export default resetPassword;
