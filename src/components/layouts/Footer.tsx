5b75b195816bc8d31aceee759b0389e0a728183e
Sylvain Guehria <sylvain.guehria@gmail.com>
2023-02-20 17:23:18 +0000

    feat(): improve footer

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -25,8 +25,8 @@ const Footer: FC = () => {
   return (
     <Background color="bg-gray-200">
       <footer>
-        <div className="mx-auto max-w-7xl overflow-hidden py-12 px-4 sm:px-6 lg:px-8">
-          <div className="mt-8 flex justify-center space-x-6">
+        <div className="mx-auto max-w-7xl py-12 px-6 md:flex md:items-center md:justify-between lg:px-8">
+          <div className="flex justify-center space-x-6 md:order-2">
             {navigation.social.map((item) => (
               <Link
                 key={item.name}
@@ -40,9 +40,11 @@ const Footer: FC = () => {
               </Link>
             ))}
           </div>
-          <p className="mt-8 text-center text-base text-gray-500">
-            &copy; 2020 Inventory Market, Inc. All rights reserved.
-          </p>
+          <div className="mt-8 md:order-1 md:mt-0">
+            <p className="text-center text-base leading-5 text-gray-500">
+              &copy; 2020 Inventory Market, Inc. All rights reserved.
+            </p>
+          </div>
         </div>
       </footer>
     </Background>
