962a514eb29dce0ddb6b140845c2378ba605bb2b
Sylvain Guehria <sylvain.guehria@gmail.com>
2023-02-21 14:25:16 +0000

    feat(): ça me gave

--- src/usecases/auth/authUtils.ts
+++ src/usecases/auth/authUtils.ts
@@ -4,7 +4,7 @@ export const getURL = () => {
   let url =
     process?.env?.NEXT_PUBLIC_SITE_URL ||
     process?.env?.NEXT_PUBLIC_VERCEL_URL ||
-    'http://localhost:3000/';
+    'tagrossedarone';
 
   url = url.includes('http') ? url : `https://${url}`;
 
