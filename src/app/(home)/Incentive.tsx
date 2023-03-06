70042e150ab6e11797b6ae5ffbe0bb4bb84a73a3
Sylvain Guehria <sylvain.guehria@gmail.com>
2023-03-06 13:38:43 +0000

    feat(): remove class that cause scroll Y

--- src/app/(home)/Incentive.tsx
+++ src/app/(home)/Incentive.tsx
@@ -29,7 +29,7 @@ const incentives = [
 ];
 
 const Incentive = () => (
-  <Background color="bg-white" className="w-screen">
+  <Background color="bg-white">
     <Section maxWidth="max-w-7xl">
       <div className="mx-auto max-w-2xl lg:max-w-7xl">
         <dl className="grid max-w-xl grid-cols-1 gap-y-10 gap-x-8 lg:max-w-none lg:grid-cols-3 lg:gap-y-16">
