38c2f8f8f539c0eb24947f0aa3156c229c907961
Sylvain Guehria <sylvain.guehria@gmail.com>
2023-03-07 17:04:58 +0000

    feat(): add subtitle in the incentive section

--- src/app/(home)/Incentive.tsx
+++ src/app/(home)/Incentive.tsx
@@ -9,19 +9,21 @@ import Section from '@/components/lib/layout/Section';
 
 const incentives = [
   {
-    name: 'Gérez votre inventory',
+    title: 'Gérez votre inventory',
     icon: CubeIcon,
     description:
       'Vous gérez simplement et efficacement votre inventaire directement sur votre téléphone, tablette ou ordinateur.',
   },
   {
-    name: 'Page web auto générée',
+    title: 'Page web auto générée',
+    subtitle: 'Bientôt disponible...',
     icon: ArrowPathIcon,
     description:
       "Une page web se créée automatiquement sur notre marketplace. Vous pouvez la customiser. Elle n'est  visible par le public que si vous le souhaitez.",
   },
   {
-    name: 'Achat',
+    title: 'Achat',
+    subtitle: 'Bientôt disponible...',
     icon: CreditCardIcon,
     description:
       "Les visiteurs peuvent consulter vos produits publics. Ils peuvent acheter directement vos produits qu'ils viendront récupérer en magasin.",
@@ -34,7 +36,7 @@ const Incentive = () => (
       <div className="mx-auto max-w-2xl lg:max-w-7xl">
         <dl className="grid max-w-xl grid-cols-1 gap-y-10 gap-x-8 lg:max-w-none lg:grid-cols-3 lg:gap-y-16">
           {incentives.map((incentive) => (
-            <div key={incentive.name} className="relative pl-16">
+            <div key={incentive.title} className="relative pl-16">
               <dt className="text-base font-semibold leading-7 text-gray-900">
                 <div className="absolute top-0 left-0 flex h-10 w-10 items-center justify-center rounded-lg bg-primary-600">
                   <incentive.icon
@@ -42,8 +44,11 @@ const Incentive = () => (
                     aria-hidden="true"
                   />
                 </div>
-                {incentive.name}
+                {incentive.title}
               </dt>
+              <span className="text-sm font-medium leading-5 text-gray-500">
+                {incentive.subtitle}
+              </span>
               <dd className="mt-2 text-base leading-7 text-gray-600">
                 {incentive.description}
               </dd>
