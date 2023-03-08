64622c8e32a4f34875fdabb2bc965efaa4e0b88d
Sylvain Guehria <sylvain.guehria@gmail.com>
2023-03-08 07:00:55 +0000

    feat(): styling input

--- src/components/lib/inputs/Input.tsx
+++ src/components/lib/inputs/Input.tsx
@@ -60,7 +60,7 @@ const Input: FC<Props> = ({
         {...rest}
         className={clsx(
           inputClassName || '',
-          'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-primary-500 focus:ring-primary-500 sm:text-sm',
+          'mt-1 block w-full rounded-md border-gray-300 bg-white text-gray-700 shadow-sm focus:border-primary-500 focus:ring-primary-500 sm:text-sm',
           'truncate hover:text-clip',
           inputWidthClassOnHover
         )}
