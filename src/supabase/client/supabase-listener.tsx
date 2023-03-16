2ee36e7f15481d9c1f8e3ea16764cfac428684a1
Sylvain Guehria <sylvain.guehria@gmail.com>
2023-03-16 17:04:42 +0000

    feat(): disabled reset password event (breaks the build)

--- src/supabase/client/supabase-listener.tsx
+++ src/supabase/client/supabase-listener.tsx
@@ -1,25 +1,12 @@
 'use client';
 
-import dynamic from 'next/dynamic';
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 import { useAuth } from '@/hooks/useAuth';
 import type { User } from '@/modules/user/userType';
 
 import { useSupabase } from './SupabaseProvider';
-
-const DynamicModal = dynamic(() => import('src/components/lib/modal/Modal'), {
-  suspense: true,
-});
-
-const DynamicResetPassword = dynamic(
-  () =>
-    import('src/app/reset-password/(reset-password-components)/ResetPassword'),
-  {
-    suspense: true,
-  }
-);
 
 export default function SupabaseListener({
   serverAccessToken,
@@ -31,8 +18,6 @@ export default function SupabaseListener({
   const { supabase } = useSupabase();
   const router = useRouter();
   const { setUserTypeUser } = useAuth();
-  const [isResetPasswordModalOpen, setIsResetPasswordModalOpen] =
-    useState(false);
 
   useEffect(() => {
     setUserTypeUser(user as User);
@@ -41,11 +26,11 @@ export default function SupabaseListener({
   useEffect(() => {
     const {
       data: { subscription },
-    } = supabase.auth.onAuthStateChange((event, session) => {
-      if (event === 'PASSWORD_RECOVERY') {
-        setIsResetPasswordModalOpen(true);
-        return;
-      }
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      // if (event === 'PASSWORD_RECOVERY') {
+      //   setIsResetPasswordModalOpen(true);
+      //   return;
+      // }
       if (session?.access_token !== serverAccessToken) {
         window.location.reload();
       }
@@ -56,12 +41,5 @@ export default function SupabaseListener({
     };
   }, [serverAccessToken, router, supabase]);
 
-  return isResetPasswordModalOpen ? (
-    <DynamicModal
-      open={isResetPasswordModalOpen}
-      handleCloseModal={() => setIsResetPasswordModalOpen(false)}
-    >
-      <DynamicResetPassword />
-    </DynamicModal>
-  ) : null;
+  return null;
 }
