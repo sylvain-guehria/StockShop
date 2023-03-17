288cb7d8cdbad007a4880d31186418fcf8132073
Sylvain Guehria <sylvain.guehria@gmail.com>
2023-03-17 15:30:52 +0000

    feat(): build test

--- src/supabase/client/supabase-listener.tsx
+++ src/supabase/client/supabase-listener.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import type { SupabaseClient } from '@supabase/supabase-js';
 import { useEffect } from 'react';
 
 import { useAuth } from '@/hooks/useAuth';
 import type { User } from '@/modules/user/userType';
+import type { Database } from '@/types/supabase';
 
 import { useSupabase } from './SupabaseProvider';
 
@@ -16,7 +17,6 @@ export default function SupabaseListener({
   user?: User;
 }) {
   const { supabase } = useSupabase();
-  const router = useRouter();
   const { setUserTypeUser } = useAuth();
 
   useEffect(() => {
@@ -26,11 +26,10 @@ export default function SupabaseListener({
   useEffect(() => {
     const {
       data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      // if (event === 'PASSWORD_RECOVERY') {
-      //   setIsResetPasswordModalOpen(true);
-      //   return;
-      // }
+    } = supabase.auth.onAuthStateChange(async (event, session) => {
+      if (event === 'PASSWORD_RECOVERY' && typeof window !== 'undefined') {
+        await updatePassword(supabase);
+      }
       if (session?.access_token !== serverAccessToken) {
         window.location.reload();
       }
@@ -39,7 +38,23 @@ export default function SupabaseListener({
     return () => {
       subscription.unsubscribe();
     };
-  }, [serverAccessToken, router, supabase]);
+  }, [serverAccessToken, supabase]);
 
   return null;
+}
+
+async function updatePassword(supabase: SupabaseClient<Database>) {
+  // eslint-disable-next-line no-alert
+  const newPassword = prompt(
+    'Choisissez un nouveau mot de passe pour votre compte'
+  );
+  const { data, error } = await supabase.auth.updateUser({
+    password: newPassword as string,
+  });
+
+  // eslint-disable-next-line no-alert
+  if (data) alert('Votre mot de passe a été mis à jour.');
+  if (error)
+    // eslint-disable-next-line no-alert
+    alert('Une erreur est survenue lors de la mise à jour du mot de passe.');
 }
