b5b9a1d1573d0de90cf3bb5f971d87bb0d8b5eb8
Sylvain Guehria <sylvain.guehria@gmail.com>
2023-03-20 16:12:17 +0000

    Revert "feat(): prompt when pass word recovery"
    This reverts commit 1ea079df1d85d898e6507e57de01ac1ae460e57f.

--- src/supabase/client/supabase-listener.tsx
+++ src/supabase/client/supabase-listener.tsx
@@ -1,13 +1,25 @@
 'use client';
 
-import type { SupabaseClient } from '@supabase/supabase-js';
-import { useEffect } from 'react';
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 import { useAuth } from '@/hooks/useAuth';
 import type { User } from '@/modules/user/userType';
-import type { Database } from '@/types/supabase';
 
 import { useSupabase } from './SupabaseProvider';
+
+const DynamicModal = dynamic(() => import('src/components/lib/modal/Modal'), {
+  suspense: true,
+});
+
+const DynamicResetPassword = dynamic(
+  () =>
+    import('src/app/reset-password/(reset-password-components)/ResetPassword'),
+  {
+    suspense: true,
+  }
+);
 
 export default function SupabaseListener({
   serverAccessToken,
@@ -17,7 +29,10 @@ export default function SupabaseListener({
   user?: User;
 }) {
   const { supabase } = useSupabase();
+  const router = useRouter();
   const { setUserTypeUser } = useAuth();
+  const [isResetPasswordModalOpen, setIsResetPasswordModalOpen] =
+    useState(false);
 
   useEffect(() => {
     setUserTypeUser(user as User);
@@ -26,9 +41,10 @@ export default function SupabaseListener({
   useEffect(() => {
     const {
       data: { subscription },
-    } = supabase.auth.onAuthStateChange(async (event, session) => {
-      if (event === 'PASSWORD_RECOVERY' && typeof window !== 'undefined') {
-        await updatePassword(supabase);
+    } = supabase.auth.onAuthStateChange((event, session) => {
+      if (event === 'PASSWORD_RECOVERY') {
+        setIsResetPasswordModalOpen(true);
+        return;
       }
       if (session?.access_token !== serverAccessToken) {
         window.location.reload();
@@ -38,22 +54,14 @@ export default function SupabaseListener({
     return () => {
       subscription.unsubscribe();
     };
-  }, [serverAccessToken, supabase]);
+  }, [serverAccessToken, router, supabase]);
 
-  return null;
-}
-
-async function updatePassword(supabase: SupabaseClient<Database>) {
-  const newPassword = window.prompt(
-    'Choisissez un nouveau mot de passe pour votre compte'
-  );
-  const { data, error } = await supabase.auth.updateUser({
-    password: newPassword as string,
-  });
-
-  if (data) window.alert('Votre mot de passe a été mis à jour.');
-  if (error)
-    window.alert(
-      'Une erreur est survenue lors de la mise à jour du mot de passe.'
-    );
+  return isResetPasswordModalOpen ? (
+    <DynamicModal
+      open={isResetPasswordModalOpen}
+      handleCloseModal={() => setIsResetPasswordModalOpen(false)}
+    >
+      <DynamicResetPassword />
+    </DynamicModal>
+  ) : null;
 }
