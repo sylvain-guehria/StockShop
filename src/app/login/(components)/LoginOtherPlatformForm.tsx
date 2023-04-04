e25baeb0998bcad19953a21e294e16cdc4aeaf20
Sylvain Guehria <sylvain.guehria@gmail.com>
2023-04-04 17:11:09 +0100

    feat(): linter

--- src/app/login/(components)/LoginOtherPlatformForm.tsx
+++ src/app/login/(components)/LoginOtherPlatformForm.tsx
@@ -66,14 +66,14 @@ const LoginOtherPlatformForm = () => {
       <div className="mt-6 grid grid-cols-2 gap-3">
         <div
           onClick={isMobile ? tellUSerToUseMagikLink : handleLoginGoogle}
-          className="inline-flex w-full cursor-pointer justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
+          className="inline-flex w-full cursor-pointer justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
         >
           <span className="sr-only">Sign in with Google</span>
           <GoogleSVG />
         </div>
         <div
           onClick={openMagikLinkModal}
-          className="inline-flex w-full cursor-pointer justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
+          className="inline-flex w-full cursor-pointer justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50"
         >
           <span className="sr-only">Sign in with Magik link</span>
           <EnvelopeIcon className="mr-1 h-5 w-5" />
