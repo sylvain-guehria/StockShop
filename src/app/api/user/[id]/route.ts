1a967dafe8b5141f34b68ded4c32071cc593c603
sylvain guehria <sylvain.guehria@gmail.com>
2023-09-28 17:08:50 +0200

    fix user post

---
+++ src/app/api/user/[id]/route.ts
@@ -0,0 +1,61 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { logException } from 'logger';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+
+import { TableNames } from '@/supabase/enums/tableNames';
+import type { Database } from '@/types/supabase';
+import { removeKeysWithNoValues } from '@/utils/objectUtils';
+
+export async function POST(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  const { id } = params;
+  const body = await request.json();
+
+  if (!id) {
+    NextResponse.json({ error: 'User id is mandatory to get profile' });
+    return;
+  }
+
+  const supabase = createServerComponentClient<Database>({ cookies });
+
+  const { error, data } = await supabase
+    .from(TableNames.PROFILES)
+    .update(removeKeysWithNoValues(body))
+    .eq('id', id);
+  if (error) {
+    logException(error, { when: 'updating user profile' });
+    NextResponse.json({ error: 'Error when updating user profile' });
+    return;
+  }
+  return NextResponse.json({ data });
+}
+
+// export async function GET(
+//   _request: Request,
+//   { params }: { params: { slug: string } },
+// ) {
+//   const id = params.slug;
+
+//   if (!id) {
+//     NextResponse.json({ error: 'User id is mandatory to get profile' });
+//     return;
+//   }
+
+//   const supabase = createServerComponentClient<Database>({ cookies });
+
+//   const { data: profile, error } = await supabase
+//     .from(TableNames.PROFILES)
+//     .select('*')
+//     .eq('id', id)
+//     .single();
+
+//   if (error) {
+//     logException(error, { when: 'getting user profile by id' });
+//     NextResponse.json({ error: 'Error when getting user profile by id' });
+//     return;
+//   }
+//   NextResponse.json(profile);
+// }
--- src/pages/404.tsx
+++
@@ -1,5 +0,0 @@
-import Page404 from '@/components/lib/page404/Page404';
-
-export default function Custom404() {
-  return <Page404 />;
-}
