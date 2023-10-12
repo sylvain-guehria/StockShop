6e7f898811a97085cff3548bb756e63ddbf449bf
sylvain guehria <sylvain.guehria@gmail.com>
2023-10-12 09:34:15 +0200

    remove sendinblue api

--- src/sendinblue/addContact.ts
+++ src/sendinblue/addContact.ts
@@ -1,36 +1,3 @@
-import { logException, logInConsole } from 'logger';
-
-const SibApiV3Sdk = require('sib-api-v3-sdk');
-
-const defaultClient = SibApiV3Sdk.ApiClient.instance;
-
-// Configure API key authorization: api-key
-const apiKey = defaultClient.authentications['api-key'];
-apiKey.apiKey = process.env.SENDINBLUE_API_KEY;
-
-const apiInstance = new SibApiV3Sdk.ContactsApi();
-
-interface ArgsTypes {
-  email: string;
-  listIds: number[];
-}
-
-const createSendinblueContact = async ({
-  email,
-  listIds = [1],
-}: ArgsTypes): Promise<boolean> => {
-  const createContact = new SibApiV3Sdk.CreateContact();
-  createContact.email = email;
-  createContact.listIds = listIds;
-
-  try {
-    const data = await apiInstance.createContact(createContact);
-    logInConsole(`createContact called successfully. Returned data: ${data}`);
-    return true;
-  } catch (error) {
-    logException(error);
-    return false;
-  }
-};
+const createSendinblueContact = async () => {};
 
 export default createSendinblueContact;
--- src/sendinblue/sendTransacEmail.ts
+++ src/sendinblue/sendTransacEmail.ts
@@ -1,29 +1,3 @@
-import { logException, logInConsole } from 'logger';
-
-/* eslint-disable no-console */
-const SibApiV3Sdk = require('sib-api-v3-sdk');
-
-const defaultClient = SibApiV3Sdk.ApiClient.instance;
-
-// Configure API key authorization: api-key
-const apiKey = defaultClient.authentications['api-key'];
-apiKey.apiKey = process.env.SENDINBLUE_API_KEY;
-
-// Uncomment below two lines to configure authorization using: partner-key
-// var partnerKey = defaultClient.authentications['partner-key'];
-// partnerKey.apiKey = 'YOUR API KEY';
-
-const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-// @ts-ignore
-const sendTransacEmail = async (sendSmtpEmail): boolean => {
-  try {
-    const data = await apiInstance.sendTransacEmail(sendSmtpEmail);
-    logInConsole(`API called successfully. Returned data: ${data}`);
-    return true;
-  } catch (error) {
-    logException(error, { when: 'error sendTransacEmail' });
-    return false;
-  }
-};
+const sendTransacEmail = async () => {};
 
 export default sendTransacEmail;
